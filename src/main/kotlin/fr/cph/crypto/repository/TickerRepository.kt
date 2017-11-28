d437d2346fcdf7539ea7c4ff4db38d56537702e2
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-11-28 10:40:48 -0600

    Add default context path

--- src/main/kotlin/fr/cph/crypto/repository/TickerRepository.kt
+++
@@ -1,7 +0,0 @@
-package fr.cph.crypto.repository
-
-import org.springframework.data.mongodb.repository.MongoRepository
-
-import fr.cph.crypto.domain.Ticker
-
-interface TickerRepository : MongoRepository<Ticker, String>
--- src/main/kotlin/fr/cph/crypto/repository/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package fr.cph.crypto.repository
-
-import org.springframework.data.mongodb.repository.MongoRepository
-
-import fr.cph.crypto.domain.User
-
-interface UserRepository : MongoRepository<User, String>
