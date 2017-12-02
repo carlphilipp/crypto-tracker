e999d26b8b3cc6e8cdbb23cf19b8de62d75de43d
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-12-02 13:01:28 -0600

    Add GRS and delete out

--- back-end/src/main/kotlin/fr/cph/crypto/DatabaseLoader.kt
+++ back-end/src/main/kotlin/fr/cph/crypto/DatabaseLoader.kt
@@ -39,18 +39,22 @@ class DatabaseLoader : CommandLineRunner {
         val btcTicker = tickerRepository.findOne("BTC-USD")
         val ethTicker = tickerRepository.findOne("ETH-USD")
         val vtcTicker = tickerRepository.findOne("VTC-USD")
+        val grsTicker = tickerRepository.findOne("GRS-USD")
 
 
         val positions = ArrayList<Position>()
         val positionBtc = Position.buildPosition(btcTicker, 0.06564277, 7616.98508457)
+        val positionEth = Position.buildPosition(ethTicker, 1.57128061, 318.2)
         val positionVtc = Position.buildPosition(vtcTicker, 122.10096277, 4.3)
-        val positionEth = Position.buildPosition(ethTicker, 1.57128061, 335.0)
+        val positionGrs = Position.buildPosition(grsTicker, 1025.10079425, 1.25)
         positions.add(positionBtc)
         positions.add(positionEth)
         positions.add(positionVtc)
+        positions.add(positionGrs)
         positionRepository.save(positionBtc)
         positionRepository.save(positionEth)
         positionRepository.save(positionVtc)
+        positionRepository.save(positionGrs)
         val user = User("cp.harmant@gmail.com", "PASSWORD", Role.ADMIN)
         user.id = "1"
         user.positions = positions
