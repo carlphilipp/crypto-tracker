2ede05f66188872915c36061ebef250365bd7572
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-12-07 23:08:05 -0600

    Delete position input options

--- front-end/src/components/modals/DeletePosition.js
+++ front-end/src/components/modals/DeletePosition.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader, Form, FormGroup, Label, Input} from 'reactstrap';
 import {deletePosition} from '../../utils/ApiClient';
 import {FormattedNumber}  from 'react-intl'
 import LoginFailure from '../LoginFailure';
@@ -41,9 +41,31 @@ class DeletePosition extends React.Component {
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
                     <ModalHeader toggle={this.toggle}>Delete</ModalHeader>
                     <ModalBody>
-                        <b>Currency:</b> {this.props.position.currency1.currencyName}<br />
-                        <b>Quantity:</b> {this.props.position.quantity}<br />
+                      <Form>
+                        <FormGroup>
+                          <Label for="currency">Currency</Label><br />
+                          {this.props.position.currency1.currencyName}
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="currency">Quantity</Label><br />
+                          {this.props.position.quantity}
+                        </FormGroup>
+                        <FormGroup>
+                          <legend>Radio Buttons</legend>
+                          <FormGroup check>
+                            <Label check>
+                              <Input type="radio" name="radio1" />{' '} Option one is this and that—be sure to include why its great
+                            </Label>
+                          </FormGroup>
+                          <FormGroup check>
+                            <Label check>
+                              <Input type="radio" name="radio1" />{' '}
+                              Option two can be something else and selecting it will deselect option one
+                            </Label>
+                          </FormGroup>
+                        </FormGroup>
                         <b>Value:</b> <FormattedNumber value={this.props.position.value} style={`currency`} currency="USD"/><br />
+                      </Form>
                     </ModalBody>
                       {/* FIXME: create a state failure*/}
                     {(this.state.failure) ? <LoginFailure /> : ''}
