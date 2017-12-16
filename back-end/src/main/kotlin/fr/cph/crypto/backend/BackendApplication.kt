094d3435778c441608f0971a6c13deee3d752510
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-12-16 16:12:15 -0600

    Improve db config

--- back-end/src/main/kotlin/fr/cph/crypto/backend/BackendApplication.kt
+++ back-end/src/main/kotlin/fr/cph/crypto/backend/BackendApplication.kt
@@ -3,10 +3,8 @@ package fr.cph.crypto.backend
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableMongoRepositories(basePackages = ["fr.cph.crypto.backend.repository"]) // temp solution time to move repo into right subproject
 @ComponentScan(value = ["fr.cph.crypto"])
 @EnableScheduling
 @SpringBootApplication
