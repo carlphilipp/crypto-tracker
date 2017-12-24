9862b0192fca127b5bee6618507a2e62e8a3ec0d
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-12-24 09:31:56 -0600

    Send email in a new thread

--- back-end/infra/email/default/src/main/kotlin/fr/cph/crypto/email/DefaultEmailAdapter.kt
+++ back-end/infra/email/default/src/main/kotlin/fr/cph/crypto/email/DefaultEmailAdapter.kt
@@ -15,38 +15,39 @@ class DefaultEmailAdapter(private val emailProperties: EmailProperties) : EmailS
     private val sslFactory = "javax.net.ssl.SSLSocketFactory"
 
     override fun sendWelcomeEmail(email: Email) {
+        Thread({
+            val props = Properties()
+            props.put("mail.smtp.host", emailProperties.server.host)
+            props.put("mail.smtp.auth", "true")
+            props.put("mail.smtp.port", emailProperties.server.port)
+            props.put("mail.smtp.socketFactory.port", emailProperties.server.port)
+            props.put("mail.smtp.socketFactory.class", sslFactory)
+            props.put("mail.smtp.socketFactory.fallback", "false")
+            props.put("mail.smtp.ssl.enable", true)
 
-        val props = Properties()
-        props.put("mail.smtp.host", emailProperties.server.host)
-        props.put("mail.smtp.auth", "true")
-        props.put("mail.smtp.port", emailProperties.server.port)
-        props.put("mail.smtp.socketFactory.port", emailProperties.server.port)
-        props.put("mail.smtp.socketFactory.class", sslFactory)
-        props.put("mail.smtp.socketFactory.fallback", "false")
-        props.put("mail.smtp.ssl.enable", true)
+            val session = Session.getDefaultInstance(props, object : Authenticator() {
+                override fun getPasswordAuthentication(): PasswordAuthentication {
+                    return PasswordAuthentication(emailProperties.email.username, emailProperties.email.password)
+                }
+            })
 
-        val session = Session.getDefaultInstance(props, object : Authenticator() {
-            override fun getPasswordAuthentication(): PasswordAuthentication {
-                return PasswordAuthentication(emailProperties.email.username, emailProperties.email.password)
-            }
-        })
+            session.debug = false
 
-        session.debug = false
+            val msg = MimeMessage(session)
+            val addressFrom = InternetAddress(emailProperties.email.from)
+            msg.setFrom(addressFrom)
 
-        val msg = MimeMessage(session)
-        val addressFrom = InternetAddress(emailProperties.email.from)
-        msg.setFrom(addressFrom)
+            val addressTo = arrayOfNulls<InternetAddress>(1)
+            addressTo[0] = InternetAddress(email.to)
+            msg.setRecipients(Message.RecipientType.TO, addressTo)
 
-        val addressTo = arrayOfNulls<InternetAddress>(1)
-        addressTo[0] = InternetAddress(email.to)
-        msg.setRecipients(Message.RecipientType.TO, addressTo)
-
-        // Setting the Subject and Content Type
-        msg.subject = email.subject
-        msg.setContent(email.content, "text/html")
-        msg.sentDate = Date()
-        LOGGER.debug("Sending email to [{}]", email.to)
-        Transport.send(msg)
+            // Setting the Subject and Content Type
+            msg.subject = email.subject
+            msg.setContent(email.content, "text/html")
+            msg.sentDate = Date()
+            LOGGER.debug("Sending email to [{}]", email.to)
+            Transport.send(msg)
+        }).start()
     }
 
     companion object {
