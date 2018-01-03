802bf849e9892fb32046ba4059856b510e7ee526
Carl-Philipp Harmant <cp.harmant@gmail.com>
2018-01-03 14:08:09 -0600

    Move back spring exception handling in application

--- back-end/application/src/main/kotlin/fr/cph/crypto/exception/RestResponseEntityExceptionHandler.kt
+++ back-end/application/src/main/kotlin/fr/cph/crypto/exception/RestResponseEntityExceptionHandler.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fr.cph.crypto.rest.exception
+package fr.cph.crypto.exception
 
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
